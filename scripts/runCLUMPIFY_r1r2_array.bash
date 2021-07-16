327960b3cbc70d4309c220ce10d4f627f10434a3
cbird808 <chris.bird@tamucc.edu>
2021-07-16 15:00:52 -0500

    Update runCLUMPIFY_r1r2_array.bash

--- scripts/runCLUMPIFY_r1r2_array.bash
+++ scripts/runCLUMPIFY_r1r2_array.bash
@@ -8,8 +8,10 @@ INDIR=$1
 OUTDIR=$2
 TMPDIR=$3
 
+SCRIPTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
 all_samples=$(ls $INDIR/$FQPATTERN | \
 	sed -e 's/r1\.fq\.gz//' -e 's/.*\///g')
 all_samples=($all_samples)
 
-sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} runCLUMPIFY_r1r2_array.sbatch ${INDIR} ${OUTDIR} ${TMPDIR} ${FQPATTERN}
+sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} $SCRIPTPATH/runCLUMPIFY_r1r2_array.sbatch ${INDIR} ${OUTDIR} ${TMPDIR} ${FQPATTERN}
