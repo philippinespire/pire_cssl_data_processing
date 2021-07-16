6d414c89e81fa7892b97eec85127e7c496c257f2
cbird808 <chris.bird@tamucc.edu>
2021-07-16 14:42:52 -0500

    Update runCLUMPIFY_r1r2_array.bash

--- scripts/runCLUMPIFY_r1r2_array.bash
+++ scripts/runCLUMPIFY_r1r2_array.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 #Pass in the maximum number of nodes to use at once
-nodes=$1
+nodes=$4
 
 FQPATTERN=*r1.fq.gz
-INDIR=
-OUTDIR=
+INDIR=$1
+OUTDIR=$2
+TMPDIR=$3
 
 all_samples=$(ls $INDIR/$FQPATTERN | \
 	sed -e 's/r1\.fq\.gz//' -e 's/.*\///g')
 all_samples=($all_samples)
 
-sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} runCLUMPIFY_r1r2_array.sbatch ${FQPATTERN} ${INDIR} ${OUTDIR}
+sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} runCLUMPIFY_r1r2_array.sbatch ${INDIR} ${OUTDIR} ${TMPDIR} ${FQPATTERN} 
