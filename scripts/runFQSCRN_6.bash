85ec65d8ad14ef9ae6db1b482a604389f2909cbb
cbird808 <chris.bird@tamucc.edu>
2021-07-21 09:19:51 -0500

    Update runFQSCRN_6.bash

--- scripts/runFQSCRN_6.bash
+++ scripts/runFQSCRN_6.bash
@@ -4,7 +4,8 @@
 
 INDIR=$1
 OUTDIR=$2
-NUMNODES=$3   # 5 to 10 on tamucc
+TMPDIR=$3
+NUMNODES=$4   # 5 to 10 on tamucc
 FQPATTERN=*.fq.gz
 
 mkdir $OUTDIR
@@ -15,7 +16,7 @@ JOBID=$(sbatch --array=0-$((${#all_samples[@]}-1))%${NUMNODES} \
        --output=slurm-fqscrn.%A.%a.out \
        --partition normal,gpu,cbirdq,main \
        -t 96:00:00 \
-       runFQSCRN_6.sbatch ${INDIR} ${OUTDIR} ${FQPATTERN})
+       runFQSCRN_6.sbatch ${INDIR} ${OUTDIR} ${TMPDIR} ${FQPATTERN})
 NUMBER1=$(echo ${JOBID} | sed 's/[^0-9]*//g')
 
 #Run Multiqc after array finishes
