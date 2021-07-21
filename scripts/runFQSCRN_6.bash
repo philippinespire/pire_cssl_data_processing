9185301d7302b008f1548052e564a7837540943a
cbird808 <chris.bird@tamucc.edu>
2021-07-21 09:18:13 -0500

    Update runFQSCRN_6.bash

--- scripts/runFQSCRN_6.bash
+++ scripts/runFQSCRN_6.bash
@@ -2,11 +2,10 @@
 
 # this script sends several jobs each to their own compute node using an array, which limits the number of nodes used at one time
 
-NUMNODES=$1   # 5 to 10 on tamucc
-
+INDIR=$1
+OUTDIR=$2
+NUMNODES=$3   # 5 to 10 on tamucc
 FQPATTERN=*.fq.gz
-INDIR=
-OUTDIR=
 
 mkdir $OUTDIR
 
@@ -14,8 +13,8 @@ all_samples=( $(ls $INDIR/$FQPATTERN) )
 
 JOBID=$(sbatch --array=0-$((${#all_samples[@]}-1))%${NUMNODES} \
        --output=slurm-fqscrn.%A.%a.out \
-       --partition normal,gpu,cbirdq \
-       -t 24:00:00 \
+       --partition normal,gpu,cbirdq,main \
+       -t 96:00:00 \
        runFQSCRN_6.sbatch ${FQPATTERN} ${INDIR} ${OUTDIR})
 NUMBER1=$(echo ${JOBID} | sed 's/[^0-9]*//g')
 
