87571324503fb02f4435e5feabfb57e129503832
cbird808 <chris.bird@tamucc.edu>
2021-07-22 11:06:40 -0500

    Update mkNewFileNames.bash

--- scripts/mkNewFileNames.bash
+++ scripts/mkNewFileNames.bash
@@ -5,6 +5,7 @@
 module load parallel
 
 INFILE=$1    #Lle_CaptureLibraries_SequenceNameDecode.tsv
+FQDIR=$2     #fq_fp1_clmp_fp2_fqscrn_repaired
 
 #get first two letters of seq names
 
@@ -15,7 +16,7 @@ bash  #need this for odu
 SAMPNAMES=($(cat <(tail -n +2 $INFILE | sort | uniq) <(tail -n +2 $INFILE | sort | uniq) | sort | cut -f2 | sed -e 's/-\([0-9][0-9][0-9]\)/_\1/' -e 's/\r//'))
 
 # clean up present file names to remove extraneous info and restore missing info
-SEQINFO=($(ls fq_fp1_clmp_fp2_fqscrn_repaired/*.fq.gz | sed "s/^fq_fp1.*\(${SPCODE}[AC].....\)_.*_\(L[1-9][0-9]*\)_clmp_fp2_repr/\1-\2-fp1-clmp-fp2-fqscrn-repr/"))
+SEQINFO=($(ls $FQDIR/*.fq.gz | sed "s/^fq_fp1.*\(${SPCODE}[AC].....\)_.*_\(L[1-9][0-9]*\)_clmp_fp2_repr/\1-\2-fp1-clmp-fp2-fqscrn-repr/"))
 
 # construct new names
 NEWNAMES=($(parallel --no-notice --link -kj2 "echo {1}-{2}" ::: ${SAMPNAMES[@]} ::: ${SEQINFO[@]} ))
