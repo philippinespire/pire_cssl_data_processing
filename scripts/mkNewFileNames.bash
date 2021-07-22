71bbf2045f02ee93487fc55b8a89a6b822f3669f
cbird808 <chris.bird@tamucc.edu>
2021-07-22 11:19:16 -0500

    Update mkNewFileNames.bash

--- scripts/mkNewFileNames.bash
+++ scripts/mkNewFileNames.bash
@@ -12,7 +12,6 @@ FQDIR=$2     #fq_fp1_clmp_fp2_fqscrn_repaired
 SPCODE=$(tail -n1 $INFILE | cut -c1-2)
 
 # get samp names from decode file, duplicate for r1 and r2 files, and put them in same sort order as sorted fq.gz files
-bash  #need this for odu
 SAMPNAMES=($(cat <(tail -n +2 $INFILE | sort | uniq) <(tail -n +2 $INFILE | sort | uniq) | sort | cut -f2 | sed -e 's/-\([0-9][0-9][0-9]\)/_\1/' -e 's/\r//'))
 
 # clean up present file names to remove extraneous info and restore missing info
