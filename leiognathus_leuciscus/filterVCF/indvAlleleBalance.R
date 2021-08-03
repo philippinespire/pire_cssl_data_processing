6038437cb82d61d8d5aab6a9122f47b8b509804d
cbird808 <cbird808@gmail.com>
2021-08-03 18:50:19 -0500

    insert message

---
+++ leiognathus_leuciscus/filterVCF/indvAlleleBalance.R
@@ -0,0 +1,16 @@
+
+
+
+#### Initialize ####
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
+library(magrittr)
+library(janitor)
+
+#### Set Variables ####
+alleledepthFILE = 'lle.D.ssl.Lle-C-3NR-R1R2ORPH-contam-noisolate-off.Fltr17.1.recode.100.AD.tsv'
+genolikelihoodFILE = 'lle.D.ssl.Lle-C-3NR-R1R2ORPH-contam-noisolate-off.Fltr17.1.recode.100.GL.tsv'
+genoFILE = 'lle.D.ssl.Lle-C-3NR-R1R2ORPH-contam-noisolate-off.Fltr17.1.recode.100.GT.tsv'
+
+#### READ in DATA ####
+
