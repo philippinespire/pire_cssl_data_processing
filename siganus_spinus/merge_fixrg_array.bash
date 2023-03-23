060994d5429cd073496d3f253208205b4c846e2a
mmalabag <mmalabag@wahab-01.ib.cluster>
2023-03-23 10:53:50 -0400

    Filtering and pop structure

---
+++ siganus_spinus/merge_fixrg_array.bash
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#script to fix read group for merged reads
+
+BAMDIR=$1		#directory containing merged bam files. example= /home/e1garcia/shotgun_PIRE/pire_cssl_data_processing/gazza_minuta/mkBAMmerge_copy
+
+BAMPATTERN=*-merged.ssl.Ssp-3NR-R1R2ORPH-contam-noisolate-RG.bam
+
+SCRIPTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+all_samples=$(ls $BAMDIR/$BAMPATTERN | sed -e 's/-merged\.ssl\.Ssp-3NR-R1R2ORPH-contam-noisolate-RG\.bam//' -e 's/.*\///g')
+all_samples=($all_samples)
+
+sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} $SCRIPTPATH/merge_fixrg_array.sbatch ${BAMDIR}
