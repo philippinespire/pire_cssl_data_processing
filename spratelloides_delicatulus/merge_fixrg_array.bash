da9547aa4f698c3c1909a50ff154826b6dc67345
e1garcia <e1garcia@wahab-01.ib.cluster>
2023-08-07 17:17:11 -0400

    updating Sde

---
+++ spratelloides_delicatulus/merge_fixrg_array.bash
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+#script to fix read group for merged reads
+
+BAMDIR=$1		#directory containing merged bam files. example= /home/e1garcia/shotgun_PIRE/pire_cssl_data_processing/gazza_minuta/mkBAMmerge_copy
+
+BAMPATTERN=*merged-RG.bam
+
+SCRIPTPATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+echo -e "\nSCRIPTPATH=$SCRIPTPATH\n"
+
+all_samples=$(ls $BAMDIR/$BAMPATTERN | sed -e 's/-merged-RG\.bam//' -e 's/.*\///g')
+all_samples=($all_samples)
+
+sbatch --array=0-$((${#all_samples[@]}-1))%${nodes} $SCRIPTPATH/merge_fixrg_array.sbatch ${BAMDIR}
